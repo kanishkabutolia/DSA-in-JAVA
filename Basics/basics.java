00b015a20bcb093851bcdec2fcaff289004ab66c
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-09 19:15:38 +0530

    learned basics

---
+++ Basics/basics.java
@@ -0,0 +1,7 @@
+public class basics{
+    public static void main(String[] args){
+        System.out.print("Hello Kanishka!!\n");
+        System.out.println("How are you!!");
+        System.out.println("Are u fine ?");
+    }
+}
---
+++ Basics/variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args){
+        int x=500;
+        System.out.println(x);
+
+        int y=10;
+        System.out.println(y);
+
+        int sum=x+y;
+        System.out.println("Sum is:" +sum);
+
+        x=50;
+        System.out.println(x);
+        System.out.println("Sum is:" +sum);
+    }
+
+}
