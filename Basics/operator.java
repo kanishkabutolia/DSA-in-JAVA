e9b14b67cbc5ca6a65acf640fd942e2c4b1c3b2d
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-16 20:15:27 +0530

    operator

---
+++ Basics/operator.java
@@ -0,0 +1,22 @@
+public class operator {
+    public static void main(String[] args){
+        int a=10;
+        int b=3;
+
+        //arithematic operator
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b + "\n");
+
+        //assignment operator
+        a+=2;
+        System.out.println(a);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
