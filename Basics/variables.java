d8e6d1eda98db4796b1961b4127964dc8d0751cf
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-16 18:17:45 +0530

    Variables

--- Basics/variables.java
+++ Basics/variables.java
@@ -1,17 +1,15 @@
 public class variables {
-    public static void main(String[] args){
-        int x=500;
+    public static void main(String[] args) {
+        int x = 500;
         System.out.println(x);
-
-        int y=10;
+        int y = 10;
         System.out.println(y);
-
-        int sum=x+y;
-        System.out.println("Sum is:" +sum);
-
-        x=50;
+        int sum = x + y;
+        System.out.println("Sum is: " + sum);
+        x = 50;
         System.out.println(x);
-        System.out.println("Sum is:" +sum);
+        System.out.println("Sum is: " + sum);
+        x = x + 50;
+        System.out.println(x);
     }
-
 }
