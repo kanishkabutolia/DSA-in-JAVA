ed47ef998479fc0c5a202a93b043a7e617910d37
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-30 18:21:58 +0530

    double data type

---
+++ Basics/datatype.java
@@ -0,0 +1,15 @@
+public class datatype {
+    public static void main (String[] args){
+        double x=2;
+        double y=3;
+        double sum = x+y;
+        System.out.println(sum);
+
+
+        int a=5;
+        int b=10;
+        double Sum = a/b;
+        System.out.println(sum);
+
+    }
+}
