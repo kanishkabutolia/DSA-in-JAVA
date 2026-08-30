99ac7344c590ae2b656fea0f5b43fa025f5b48c1
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-30 17:34:58 +0530

    practice ques

---
+++ Practice_ques/q1.java
@@ -0,0 +1,11 @@
+public class q1 {
+    public static void main (String [] args){
+    //Sum of two numbers
+
+    int x=10, y=15;
+    x++;
+    y--;
+    int Sum=x+y;
+    System.out.println("Sum of two var x and y is: "+ Sum);
+    }
+}
