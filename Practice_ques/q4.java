8b1f3aec4cc28779ae3b88655e2a2538be77a533
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-31 16:10:14 +0530

    practice question 4

---
+++ Practice_ques/q4.java
@@ -0,0 +1,12 @@
+public class q4 {
+    public static void main (String[] args){
+        //Calculate volume of sphere with given radius
+
+        double radius = 6.39;
+        double pi = 3.14;
+        double volume = ((double)4/3)*pi*radius*radius*radius;
+
+        System.out.printf("Volume of Sphere with the radius = %.2f is : %.2f", radius, volume);
+    }
+    
+}
