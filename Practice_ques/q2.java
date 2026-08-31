403249cdec2c2d55e6dcdb044ab00ebbd28bd580
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-31 15:17:14 +0530

    practice question 2

--- Practice_ques/q2.java
+++ Practice_ques/q2.java
@@ -2,15 +2,19 @@ public class q2 {
     public static void main(String[] args){
         // Calculate the average of 5 subjects
 
-        double x1 = 59;
-        double x2 = 92;
-        double x3 = 61;
-        double x4 = 88;
-        double x5 = 33;
-
+        double x1 = 45;
+        double x2 = 33;
+        double x3 = 55;
+        double x4 = 45;
+        double x5 = 45;
+        double sum = x1+x2+x3+x4+x5;
         double average = (x1 + x2 + x3 + x4 + x5)/5;
 
+        System.out.printf("Total Marks = %d \n", 60*5);
+        System.out.printf("Obtained marks = %.1f \n", sum);
         System.out.println("Average of 5 subjects is: " + average);
+        System.out.printf("Marks deducted: %.1f", 300-sum);
+        
 
     }
 }
