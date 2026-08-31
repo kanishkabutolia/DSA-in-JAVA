f9d1a21bc7a43997dcb30e9b9ffb1858ee268491
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-08-31 16:27:58 +0530

    practice question 5

---
+++ Practice_ques/q5.java
@@ -0,0 +1,13 @@
+public class q5{
+    public static void main (String[] args){
+        //Calculate the simple interest
+
+        double p = 1.15;
+        double r = 44;
+        double t = 7.75;
+
+        double si =  (p*r*t)/100;
+
+        System.out.printf("Simple Interest of Principal = %.2f , Rate = %.2f  & Time = %.2f is: %.2f",p,r,t,si);
+    }
+}
