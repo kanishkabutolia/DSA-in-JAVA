a0767475668a221cb35e38a42a4d6c5c866c26ff
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-09-02 12:42:48 +0530

    user input

--- input/sumof2no.java
+++ input/sumof2no.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class sumof2no {
     public static void main (String[] args){
         Scanner sc = new Scanner (System.in);
-        int a,b;
+        
         System.out.print("Enter the value of a: ");
-        a =  sc.nextInt();
+        int a =  sc.nextInt();
 
         System.out.print("Enter the value of b: ");
-        b =  sc.nextInt();
+        int b =  sc.nextInt();
 
         int Sum = a+b;
         System.out.printf("The sum of %d and %d is: %d", a,b,Sum);
