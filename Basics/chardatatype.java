5c443ffd77feba2f46c1f22d2d7023a8e8440a54
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-09-04 16:58:27 +0530

    char dtaa type

---
+++ Basics/chardatatype.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class chardatatype {
+    public static void main (String[] args){
+        // Char data type
+
+        char ch = 'a';
+        int a = ch;
+        int b = (int)ch;
+        int num = 99;
+        char c = (char)num;
+        System.out.println(ch);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
