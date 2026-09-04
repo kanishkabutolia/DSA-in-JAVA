1e7c1b6973ad9a34e88ec7190dbf17936a2f32fd
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-09-04 17:49:11 +0530

    condition formatting

---
+++ conditional formatiing/oddeven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class oddeven {
+    public static void main (String[] args){
+        //Find whether the number is odd or even!!
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        if(num%2==0){
+            System.out.println("Your entered number is even!");
+        }
+        else{
+            System.out.println("Your entered number is odd!");
+        }
+
+    }
+}
