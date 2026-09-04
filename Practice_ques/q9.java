937851f6909e29bdb4f0268a14bcecb26e48da6c
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-09-04 21:09:34 +0530

    practice question 9

---
+++ Practice_ques/q9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class q9 {
+    public static void main (String[] args){
+        // Take any integer as an input and if the integer is negative make its absolute value!
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter the integer: ");
+        int integer = sc.nextInt();
+
+        if(integer<0){
+            integer = integer * -1;
+            System.out.println(integer);
+        }
+
+    }
+}
