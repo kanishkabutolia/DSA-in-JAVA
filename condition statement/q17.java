7419276bf1c3634b110e7cab54d59c2acf8cf875
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-09-05 17:48:07 +0530

    Conditional Statement

--- condition statement/q17.java
+++ condition statement/q17.java
@@ -9,13 +9,20 @@ public class q17 {
         System.out.print("Enter the number: ");
         int num = sc.nextInt();
 
-        if(num%5==0 || num%3==0){
-            if(num%15!=0){
+        // if(num%5==0 || num%3==0){
+        //     if(num%15!=0){
+        //         System.out.println("Number is divisible by 5 or 3 but not by 15!");
+        //     } 
+        //     else{
+        //     System.out.println("The number is not divisible!!");
+        //     }
+        // }
+        // else{
+        //     System.out.println("The number is not divisible!!");
+        // }
+
+        if(num%15!=0 && (num%5==0 || num%3==0)){
                 System.out.println("Number is divisible by 5 or 3 but not by 15!");
-            } 
-            else{
-            System.out.println("The number is not divisible!!");
-            }
         }
         else{
             System.out.println("The number is not divisible!!");
