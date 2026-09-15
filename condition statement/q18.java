7e46bbf15fa1ee5da4d7441e701e1a226629bc0f
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-09-15 14:37:45 +0530

    practice question 18

--- condition statement/q18.java
+++ condition statement/q18.java
@@ -15,16 +15,33 @@ public class q18{
         System.out.print("Enter 3rd num: ");
         int num3 = sc.nextInt();
 
-        if(num1>num2 && num1>num3){
-            System.out.println(num1);
-        }
+        // if(num1>num2 && num1>num3){
+        //     System.out.println(num1);
+        // }
 
-        if(num2>num1 && num2>num3){
-            System.out.println(num2);
-        }
+        // if(num2>num1 && num2>num3){
+        //     System.out.println(num2);
+        // }
 
-        else{
-            System.out.println(num3);
+        // else{
+        //     System.out.println(num3);
+        // }
+
+        if(num1>num2){
+            if(num1>num3){
+                System.out.println(num1 + " is the greatest");
+            }
+            else{
+                System.out.println(num3 + " is the greatest");
+            }
+        }
+        else{  //num1<num2
+            if(num2>num3){
+                System.out.println(num2 + " is the greatest");
+            }
+            else{
+                System.out.println(num3 + " is the greatest");
+            }
         }
     }
 }
