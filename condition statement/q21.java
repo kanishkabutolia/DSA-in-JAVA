6fa2afb2d676149edf0efbecf1e547ea2f3c0cf6
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-09-20 23:54:01 +0530

    practice question 21

---
+++ condition statement/q21.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class q21 {
+    public static void main(String[] args){
+        //Given points (x,y), find out whether they lie on x-axis, y-axis or origin
+
+        Scanner sc = new Scanner (System.in);
+
+        int x,y;
+
+        System.out.print("Enter x: ");
+        x = sc.nextInt();
+
+        System.out.print("Enter y: ");
+        y = sc.nextInt();
+
+        if(x==0 && y==0){
+            System.out.println("Both points lies at origin (0,0)");
+        }
+        else if()
+
+    }
+}
